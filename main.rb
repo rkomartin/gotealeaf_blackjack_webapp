42778c98ebee7d5f302cd1c6092501dbc4b8bf3d
Robert Komartin <robert.komartin@gmail.com>
2013-04-27 18:41:54 +0300

    Added betting to the game

--- main.rb
+++ main.rb
@@ -10,6 +10,8 @@ SUITS = %w[hearts spades clubs diamonds]
 VALUES = %w[2 3 4 5 6 7 8 9 10 jack queen king ace]
 BLACKJACK = 21
 DEALER_LIM = 17
+PLAYER_AMOUNT = 500
+DEALER_AMOUNT = 100000
 
 # Generic card game classes
 
@@ -39,10 +41,11 @@ class Deck
 end
 
 class Player
-  attr_reader :name, :hand
+  attr_reader :name, :hand, :amount
 
-  def initialize(name)
+  def initialize(name, amount)
     @name = name
+    @amount = amount
     @hand = []
   end
 
@@ -52,6 +55,14 @@ class Player
 
   def value
     @hand.size
+  end
+
+  def bet(pot)
+    @amount -= pot
+  end
+
+  def win(pot)
+    @amount += pot
   end
 end
 
@@ -98,6 +109,21 @@ helpers do
   def show_card(card)
     "<img src='/images/cards/#{card.suit}_#{card.value}.jpg' class=card_image >"
   end
+
+  def player_win
+    session[:player].win(2*session[:bet])
+    session[:player_amount] += 2* session[:bet]
+  end
+
+  def dealer_win
+    session[:dealer].win(2*session[:bet])
+  end
+
+  def tie_win
+    session[:player].win(session[:bet])
+    session[:player_amount] += session[:bet]
+    session[:dealer].win(session[:bet])
+  end
 end
 
 get '/' do
@@ -114,13 +140,14 @@ post '/set_player_name' do
     erb :set_player_name
   else
     session[:player_name] = params['player_name']
+    session[:player_amount] = PLAYER_AMOUNT
     redirect '/start'
   end
 end
 
 get '/start' do
-  session[:player] = BJPlayer.new(session[:player_name])
-  session[:dealer] = BJDealer.new("Dealer")
+  session[:player] = BJPlayer.new(session[:player_name], session[:player_amount])
+  session[:dealer] = BJDealer.new("Dealer", DEALER_AMOUNT)
   session[:deck] = Deck.new
 
   session[:player] << session[:deck].deal
@@ -128,7 +155,28 @@ get '/start' do
   session[:player] << session[:deck].deal
   session[:dealer] << session[:deck].deal
 
-  redirect '/player_round'
+  redirect '/bet'
+end
+
+get '/bet' do
+  erb :bet
+end
+
+post '/bet' do
+  if params["bet"].nil? || params["bet"].to_i == 0
+    @error = "The amount must not be zero or empty"
+    erb :bet
+  elsif params["bet"].to_i > session[:player].amount
+    @error = "The amount must not be higher than your current account"
+    erb :bet
+  else
+    session[:bet] = params["bet"].to_i
+    session[:player_amount] -= session[:bet]
+    session[:player].bet(session[:bet])
+    session[:dealer].bet(session[:bet])
+
+    redirect '/player_round'
+  end
 end
 
 get '/player_round' do
@@ -188,11 +236,16 @@ post '/dealer_round' do
 end
 
 post '/play_again' do
-  if params['play_again'] == 'yes'
-    temp = session[:player_name]
+  if params['play_again'] == 'yes' && session[:player_amount] != 0
+    temp_n = session[:player].name
+    temp_a = session[:player].amount
     session.clear
-    session[:player_name] = temp
+    session[:player_name] = temp_n
+    session[:player_amount] = temp_a
     redirect '/start'
+  elsif params['play_again'] == 'yes' && session[:player_amount] == 0
+    @error = "We know you said Yes. But you're broken..."
+    erb :play
   elsif params['play_again'] == 'no'
     redirect '/quit'
   else
